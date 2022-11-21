f037e5e061383628f219150b53ac3297110d1155
Moreno Andrade <moreno.andrade@ifood.com.br>
2022-11-21 15:24:51 -0300

    feat: added date

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import logo from './logo.svg';
 import { result } from './db';
 import './App.css';
 
+const formatDate = date => new Intl.DateTimeFormat('pt-BR').format(date);
+
 function App() {
-  return result.map(x => (
-    <div>
+  return result.map((x, index) => (
+    <div key={index}>
       <div style={{ background: 'red' }}>
         <p>
-          <strong>Pedido cru:</strong> {x.itemDescription}
+          <strong>Pedido cru:</strong> {x?.itemDescription}
+        </p>
+        <p>
+          <strong>Data de criação:</strong> {formatDate(x?.createDate)}
+        </p>
+        <p>
+          <strong>Id consumer:</strong> {x?.idConsumer}
         </p>
       </div>
       <div>
         <strong>Pedido normalizado</strong>
         {x.NormalizedItens.map((y, index) => (
-          <>
-            {' '}
-            <div style={{ background: 'yellow' }}>
-              <p>
-                <strong>Sugestoes para: </strong> {y.descrition}
-              </p>
-              {y.Suggestions.map(x => (
-                <>
-                  <p>Produto: {x.excerpt}</p>
-                  <p>R$: {x.prices[0].price} </p>
-                  <p>Relevancia: {x.relevance}</p>
-                  -----------------------------
-                </>
-              ))}
-            </div>
-          </>
+          <div key={index} style={{ background: 'yellow' }}>
+            <p>
+              <strong>Sugestoes para: </strong> {y.descrition}
+            </p>
+            {y.Suggestions.map((x, index) => (
+              <div key={index}>
+                <p>Produto: {x.excerpt}</p>
+                <p>R$: {x.prices[0].price} </p>
+                <p>Relevancia: {x.relevance}</p>
+                -----------------------------
+              </div>
+            ))}
+          </div>
         ))}
       </div>
     </div>
